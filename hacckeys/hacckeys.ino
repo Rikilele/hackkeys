872fdfb0d0997a70bf97df135d1c9b2f41d07877
Riki Singh Khorana <rkhorana@andrew.cmu.edu>
2017-02-11 19:16:35 +0900

    updated backbone

--- hacckeys/hacckeys.ino
+++ hacckeys/hacckeys.ino
@@ -10,6 +10,7 @@
 
 // Special Keys
 
+#define PROGRAM_KEY 0
 
 /*
  *      CHAR CATCHING BY ARDUINO STARTS HERE
@@ -38,7 +39,7 @@ void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
   // introduce a mod, so things work out
   MODIFIERKEYS modif;
-  *((uint8_t*)&modif) = m;
+  *((uint8_t*)&modif) = mod;
   
   lCtrlOn = (modif.bmLeftCtrl  == 1) ? true : false;
   lShftOn = (modif.bmLeftShift == 1) ? true : false;
@@ -64,13 +65,13 @@ void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 // What gets sent to USB
 uint8_t buf[8] = { 0 };
 
-// Defining keyboard mode
-#define QWERTY   1
-#define DVORAK   2
-#define COLEMARK 3
-
-// For now
-int state = QWERTY;
+//// Defining keyboard mode
+//#define QWERTY   1
+//#define DVORAK   2
+//#define COLEMARK 3
+//
+//// For now
+//int state = QWERTY;
 
 // Function to clear input modifiers and key
 void releaseKey() {
@@ -79,10 +80,42 @@ void releaseKey() {
   Serial.write(buf, 8);
 }
 
+/*
+ *     Functions for Programmable mode (Step by Step)
+ */
+
+void determineHack() {
+  // Turn LED ON
+  while (digitalRead(PROGRAM_KEY) == 1);        // loop until program key is pressed
+  // sendKeyToDataBase();
+  delay(1000);                                  // just so things don't move too fast
+  return;
+}
+
+void determineKey() {
+  // Make LED FLASH
+  while (digitalRead(PROGRAM_KEY) == 1);        // loop until program key is pressed
+  // sendHackToDataBase();
+  return;
+}
+
+void setHackkey() {
+  determineHack();
+  determineKey();
+  return;
+}
+
 
 /*
  *     ARDUINO SETUP AND LOOP
  */
+
+// necessary to read from keyboard
+USB                                   Usb;
+USBHub                                Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+uint32_t                              next_time;
+KbdRptParser                          Prs;
 
 void setup()
 {
@@ -96,20 +129,35 @@ void setup()
   delay( 200 );
   next_time = millis() + 5000;
   HidKeyboard.SetReportParser(0, &Prs);
+
+  // set pins
+  pinMode(PROGRAM_KEY, INPUT);
+  digitalWrite(PROGRAM_KEY, 1);
+
+  delay( 200 ); //いるかどうかわからん
 }
 
 void loop() {
-
-  if (state == QWERTY) {
-
-    // alphabet first
-    buf[2] = key;
-    
-
-   
-   // これ一番最後 
-   Serial.write(buf, 8);
-   releaseKey();
+  
+  // need this for reading from keyboard
+  Usb.Task();
+  
+  // first check if user wants to enter program mode
+  int progMode = digitalRead(PROGRAM_KEY);
+  if (progMode != 1) {
+    setHackkey();
   }
+  // if not, check if user wants to use command
+  else if (1/*userWantsToCommand*/) {
+    // command will make buf different
+  }
+  else {
+    // manipulate buf as usual
+  }
+
+  // これ一番最後
+  // possible bug: if enter first if statement, then buf isn't set to a particular value
+  Serial.write(buf, 8);
+  releaseKey();
 
 }
