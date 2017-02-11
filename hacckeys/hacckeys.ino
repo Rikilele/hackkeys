09a6507966038182d4e6aa78f25fffe4055b0aa5
Paul Elder <paul.elder@amanokami.net>
2017-02-11 16:52:25 -0500

    Added debug messages and fixed a couple macro calls

--- hacckeys/hacckeys.ino
+++ hacckeys/hacckeys.ino
@@ -10,8 +10,8 @@
 #endif
 
 // Special Keys - pins
-#define LAYER_SWITCH   2
-#define PROGRAM_KEY    3
+#define LAYER_SWITCH   14
+#define PROGRAM_KEY    6
 #define RESET_KEY      5
 
 // GLOBAL Variables
@@ -28,6 +28,7 @@ boolean RESET                 = false;        // reset flag is set when reset is
 
 uint8_t prog_comm[7];
 uint8_t prog_macro[12][7];
+uint8_t prog_macro_pos = 0;
 
 uint8_t buf[8] = { 0 };
 /*
@@ -49,6 +50,7 @@ uint8_t buf[8] = { 0 };
  */
 
 boolean addMacro(){
+  Serial.println("Adding macro...");
   // Load flags
   uint16_t macro_slots;
   EEPROM.get(0, macro_slots);
@@ -66,15 +68,18 @@ boolean addMacro(){
 	EEPROM.put(addr, prog_macro[j]);
       }
       // Write success
+      Serial.println("Success!");
       return true;
     }
     // If taken, loop again
   }
   // Here, everything is taken, so return false
+  Serial.println("Failure!");
   return false;  
 }
 
 boolean execMacro(){
+  Serial.println("Executing macro...");
   bool equal = false;
   int addr = 0;
   uint8_t buffer[7];
@@ -104,15 +109,18 @@ boolean execMacro(){
 	Serial.write(buf, 8);
 	if(buf[0] == buf[1] == buf[2] == buf[3] == buf[4] == buf[5] == buf[6] == buf[7] == 0){ k = 12; }
       }
+      Serial.println("Success!");
       return true;
     }
     // If not equal, loop again
   }
   // No matching command, return false
+  Serial.println("Failure!");
   return false;
 }
 
 boolean deleteMacro(){
+  Serial.println("Deleting single macro...");
   bool equal = false;
   int addr = 0;
   uint8_t buffer[7];
@@ -132,14 +140,17 @@ boolean deleteMacro(){
       EEPROM.get(0, macro_slots);
       macro_slots &= ~(0x1 << i);
       EEPROM.put(0, macro_slots);
+      Serial.println("Success!");
       return true;
     }
     // If not equal, loop again
   }
   // No matching command, return false
-  return false;
+  Serial.println("Failure!");
+  return false;  
 }
 void deleteAllMacros(){
+  Serial.println("Deleting all macros");
   uint16_t macro_slots;
   EEPROM.get(0, macro_slots);
   macro_slots &= 0x0;
@@ -173,26 +184,44 @@ void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
     Serial.write(buf, 8);
   }
   else if (PROGRAM_MODE_COMM) {
-    // turn on LED
-    // write command to temp global
-    PROGRAM_MODE_COMM  = false;
-    PROGRAM_MODE_MACRO = true;
+    if (digitalRead(PROGRAM_KEY) != 1) {
+      prog_comm[0] = buf[0];
+      prog_comm[1] = buf[2];
+      prog_comm[2] = buf[3];
+      prog_comm[3] = buf[4];
+      prog_comm[4] = buf[5];
+      prog_comm[5] = buf[6];
+      prog_comm[6] = buf[7];
+      PROGRAM_MODE_COMM  = false;
+      PROGRAM_MODE_MACRO = true;
+    }
   }
   else if (PROGRAM_MODE_MACRO) {
-    // flash LED
-    // send key events to temp global
-    // this function should reset the flag as well
+    prog_macro[prog_macro_pos][0] = buf[0];
+    prog_macro[prog_macro_pos][1] = buf[2];
+    prog_macro[prog_macro_pos][2] = buf[3];
+    prog_macro[prog_macro_pos][3] = buf[4];
+    prog_macro[prog_macro_pos][4] = buf[5];
+    prog_macro[prog_macro_pos][5] = buf[6];
+    prog_macro[prog_macro_pos][6] = buf[7];
+    prog_macro_pos += 1;
+    //PROGRAM_MODE_MACRO = false;
   }
   else {
-    // check for comm/macro combination
-    // then,
-    Serial.write(buf, 8);
+    prog_comm[0] = buf[0];
+    prog_comm[1] = buf[2];
+    prog_comm[2] = buf[3];
+    prog_comm[3] = buf[4];
+    prog_comm[4] = buf[5];
+    prog_comm[5] = buf[6];
+    prog_comm[6] = buf[7];
+    if(!execMacro())
+      Serial.write(buf, 8);
   }
   
 }
 
 void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key) {
-  // TODO: Macro mode should be including key up
   buf[0] = 0;
   if(buf[2] == key){ buf[2] = 0; }
   else if(buf[3] == key){ buf[3] = 0; }
@@ -200,7 +229,25 @@ void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key) {
   else if(buf[5] == key){ buf[5] = 0; }
   else if(buf[6] == key){ buf[6] = 0; }
   else if(buf[7] == key){ buf[7] = 0; }
-  Serial.write(buf, 8);
+
+  if(!PROGRAMMABLE_LAYER){
+    Serial.write(buf, 8);
+  }
+  else if (PROGRAM_MODE_MACRO) {
+    prog_macro[prog_macro_pos][0] = buf[0];
+    prog_macro[prog_macro_pos][1] = buf[2];
+    prog_macro[prog_macro_pos][2] = buf[3];
+    prog_macro[prog_macro_pos][3] = buf[4];
+    prog_macro[prog_macro_pos][4] = buf[5];
+    prog_macro[prog_macro_pos][5] = buf[6];
+    prog_macro[prog_macro_pos][6] = buf[7];
+    prog_macro_pos += 1;
+    //PROGRAM_MODE_MACRO = false;
+  }
+  else {
+    Serial.write(buf, 8);
+  }
+
 }
 
 
@@ -254,14 +301,7 @@ void setup()
   digitalWrite(PROGRAM_KEY, 1);
   pinMode(LED_PROG, OUTPUT);
 
-  /*uint64_t num;
-  boolean cont = true;
-  while(cont && addr < EEPROM.length()){
-    EEPROM.get(addr, num);
-    if(num == 0xffffffffffffffff){ cont = false; }
-    else{ addr += 1; }
-    }*/
-
+  Serial.println("Done starting");
   delay( 200 );
 }
 
@@ -275,8 +315,10 @@ void loop() {
   if (PROGRAMMABLE_LAYER) {
     
     if (digitalRead(RESET_KEY) != 1) {     // read reset key first
+      Serial.println("reset key pressed!");
       if (RESET) {
         deleteAllMacros();                 // if second time pressing reset, reset all programs
+	RESET = false;
       }
       else {
         RESET = true;                      // set up reset flag
@@ -288,6 +330,21 @@ void loop() {
     }
   }
 
+  if(digitalRead(RESET_KEY) && PROGRAM_MODE_MACRO){
+    PROGRAM_MODE_MACRO = false;
+    deleteMacro();
+  }
+  
+  if(digitalRead(PROGRAM_KEY) && PROGRAM_MODE_MACRO) {
+    PROGRAM_MODE_MACRO = false;
+    addMacro();
+  }
+  
+  if(prog_macro_pos == 11){
+    PROGRAM_MODE_MACRO = false;
+    addMacro();
+  }
+  
   /*
   *     LED task
   */
@@ -317,4 +374,12 @@ void loop() {
   }
   // set the LED with the ledState of the variable:
   digitalWrite(LED_PROG, ledState);
+
+
+
+
+  Serial.println(PROGRAMMABLE_LAYER);
+  Serial.println(PROGRAM_MODE_COMM);
+  Serial.println(PROGRAM_MODE_MACRO);
+  Serial.println(RESET);
 }
